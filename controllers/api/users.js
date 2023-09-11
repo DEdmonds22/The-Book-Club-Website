0a2f398f6649bf60404b0c61f6e94911dc53b1ad
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-10 22:02:58 -0400

    erased user mock up, and connected it to the db to create an actual user

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,14 +1,13 @@
+import User from "../../models/user";
+
 // This serves as the create Sign-Up controller Action. This will need to return a JSON JWT from the controller action after the user is added to the db.
-const create = (req, res) => {
-    res.json({
-        user: {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            userName: req.body.userName
-        }
-    })
-}
+const create = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+    } catch (error) {
+        res.status(400).json(error);
+    };
+};
 
 module.exports = {
     create
