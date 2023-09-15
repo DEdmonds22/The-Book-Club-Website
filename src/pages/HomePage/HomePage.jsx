52de9c2e9f0ebe4ad2032c60f738e4b14f0bb132
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-15 18:44:25 -0400

    completed basic layout and design of homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styles from "../HomePage/homePage.css"
 
 export default function HomePage({user}) {
     return (
