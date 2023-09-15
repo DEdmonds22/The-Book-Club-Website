241b3483b99451b19b4b3b1b34851f66503d2555
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-15 19:37:34 -0400

    set the user state whenever the page is loaded or refreshed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import HomePage from "./pages/HomePage/HomePage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import SearchResults from "./pages/SearchResults/SearchResults";
+import { getUser } from "./utilities/users-service";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
+  const [bookList, setBookList] = useState([]); // will be passed to bookshelf page, when it's created. It will list all books added to bookshelf.
 
   return (
     <div className="App">
@@ -14,6 +17,7 @@ function App() {
         <NavBar user={user} />
         <Routes>
           <Route path="/" element={<HomePage user={user} />} />
+          <Route path="/searchResults/" element={<SearchResults setBookList={setBookList} />} />
         </Routes>
       </> :
       <LandingPage setUser={setUser} />}
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,15 +2,14 @@ import * as usersAPI from "./users-api";
 
 export function getToken() {
     const token = localStorage.getItem('token');
-    console.log(token)
     if (!token) return null;  // if no token found in user's local storage, return setUser(null);
 
     const payload = JSON.parse(atob(token.split(".")[1]));
     if (payload.exp < Date.now()/1000) {  //if token has expired, it is removed, also returns setUser(null);
         localStorage.removeItem("token");
         return null;
-    }
-    
+    };
+
     return token;  // otherwise, return the token
 }
 
