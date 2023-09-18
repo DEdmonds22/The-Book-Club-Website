46792af4a26d6b3d1ce6ee50b998e8cd67e3a22e
DEdmonds22 <ddedmonds22@gmail.com>
2023-09-17 21:07:36 -0400

    minor changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,19 +5,19 @@ export default function HomePage({user}) {
     return (
         <main id="home-page">
         <main id="container">
-            <div class="bookOfTheMonth top">
+            <div className="bookOfTheMonth top">
                 <Link to="/book-of-the-month" >
                     <h2>Book of The Month</h2>
                 </Link>
             </div>
 
-            <div class="newlyCreatedClubs top">
+            <div className="newlyCreatedClubs top">
                 <Link to="/newly-created-clubs" >
                     <h2>Newly Created Clubs!</h2>
                 </Link>
             </div>
 
-            <div class="profile">
+            <div className="profile">
                 <Link to="/profile" >
                     <h2>Profile</h2>
                 </Link>
@@ -25,25 +25,25 @@ export default function HomePage({user}) {
                 <h4>Book Shelf</h4>
             </div>
 
-            <div class="bookClubDiscussionBoard top">
+            <div className="bookClubDiscussionBoard top">
                 <Link to="/discussion-board" >
                     <h2>Book Club Discussion Board</h2>
                 </Link>
             </div>
 
-            <div class="recommendedBooks">
+            <div className="recommendedBooks">
                 <Link to="/recommended-books" >
                     <h2>Recommended Books</h2>
                 </Link>
             </div>
 
-            <div class="hosting bottom">
+            <div className="hosting bottom">
                 <Link to="/hosting" >
                     <h2>Hosting</h2>
                 </Link>
             </div>
 
-            <div class="upcommingEvents bottom">
+            <div className="upcommingEvents bottom">
                 <Link to="/upcomming-events" >
                     <h2>Upcomming Events</h2>
                 </Link>
